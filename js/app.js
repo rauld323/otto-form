8701379d110e2d5770ea69d526d9beed63ec5bce
Raul <rauldavila323@gmail.com>
2021-04-18 22:09:26 +0200

    Hamburger Menu has been added and styled.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-const localFormat = 'en-EN'; //format can be changed depeding on country standard
+//format can be changed depeding on country standard
+const localFormat = 'en-EN';
 const selectElement = document.getElementById('timeslots');
 
-
-
+// Call json file
 fetch("dates.json")
 	.then(response => response.json())
 	.then(data => {
@@ -12,9 +12,12 @@ fetch("dates.json")
 
 		for (let i = 0; i < data.result.length; i++) {
 
+			//Create Start and End Dates
 			const startDate = new Date(data.result[i].start);
 			const endDate = new Date(data.result[i].end);
 
+
+			//Can use 'full, long, medium, short' to change the time Style
 			const start = new Intl.DateTimeFormat(localFormat, {
 				dateStyle: 'long',
 				timeStyle: 'short'
@@ -24,22 +27,29 @@ fetch("dates.json")
 				timeStyle: 'short'
 			}).format(endDate);
 
+
 			const formatted = `${start} - ${end}`;
 
-			const opt = document.createElement('option');
+			// An option element has been created
+			const option = document.createElement('option');
 
-			opt.innerHTML = formatted;
-			opt.value = i;
+			option.innerHTML = formatted;
+			option.value = i;
 
-			selectElement.appendChild(opt);
+			selectElement.appendChild(option);
 		}
-
 
 	})
 
 
+//Hamburger Menu
+function openNav() {
+	document.getElementById("mySidepanel").style.width = "100%";
+}
 
-
+function closeNav() {
+	document.getElementById("mySidepanel").style.width = "0";
+}
 
 
 
